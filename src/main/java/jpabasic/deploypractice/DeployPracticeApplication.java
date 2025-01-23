32e821e783237851da36d1e89428b6ce082fbf5b
1224kang <cherry1224@sookmyung.ac.kr>
2025-01-23 12:44:32 +0900

    hello java

--- src/main/java/jpabasic/deploypractice/DeployPracticeApplication.java
+++ src/main/java/jpabasic/deploypractice/DeployPracticeApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeployPracticeApplication {
 
+
     public static void main(String[] args) {
+        System.out.println("hello java");
         SpringApplication.run(DeployPracticeApplication.class, args);
     }
 
