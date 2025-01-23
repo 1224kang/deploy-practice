94e3f7fd3735dfd93bce09a4ee83a4741cbe1d44
1224kang <cherry1224@sookmyung.ac.kr>
2025-01-23 14:37:36 +0900

    error 변경

--- src/main/java/jpabasic/deploypractice/Controller.java
+++ src/main/java/jpabasic/deploypractice/Controller.java
@@ -1,17 +1,18 @@
 package jpabasic.deploypractice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @ControllerAdvice
 public class Controller {
 
-    @ControllerAdvice
-    public class GlobalExceptionHandler {
-        @RequestMapping("/error")
-        public String handleError() {
-            return "error";  // error.html을 반환
-        }
+    
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error"; // error.html을 반환
     }
 }
